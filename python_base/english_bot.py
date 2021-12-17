ea19077738275e48773aa8234e7e83f4d2b6842d
Mariia <mariia.gorobets@takeoff.com>
2021-12-17 23:48:13 +0200

    Implement functions

--- python_base/english_bot.py
+++ python_base/english_bot.py
@@ -20,55 +20,33 @@ english_sentences = [
 ]
 
 user = {"username": "Kate",
-        "level": 2}
+        "level": 1}
 
-input_word = "The"
-input_word = input_word.upper()
-message = ""
-for sentence in english_sentences:
-    if user.get("level") == sentence.get("level"):
-        if input_word in sentence.get("text").upper():
-            message = message + sentence.get("text") + "\n*****\n"
-if not message:
-    print("Sorry, no sentences found.")
-else:
-    print(message)
+def filter_out_sentences_by_level():
+    level = user.get("level")
+    filtered_sentences = []
+    for sentence in english_sentences:
+        if type(sentence) == dict:
+            if level == int(sentence["level"]):
+                filtered_sentences.append(sentence)
+        else:
+            continue
+    return filtered_sentences
 
-###############################################
-english_sentences_damaged = [
-    {"text": "When my time comes \nForget the wrong that I’ve done.",
-     "level": 1},
-    "In a hole in the ground there lived a hobbit.",
-    {"text": "The sky the port was the color of television, tuned to a dead channel.",
-     "level": "4"},
-    {"text": "I love the smell of napalm in the morning.",
-     "level": 0},
-    {"text": "The man in black fled across the desert, and the gunslinger followed.",
-     "level": 0},
-    {"text": "The Consul watched as Kassad raised the death wand.",
-     "level": 1},
-    {"text": "If you want to make enemies, try to change something.",
-     "level": 2},
-    {"text": "We're not gonna take it. \nOh no, we ain't gonna take it \nWe're not gonna take it anymore",
-     "level": 1},
-    {"text": "I learned very early the difference between knowing the name of something and knowing something.",
-     "level": 2}
-]
-
-user = {"username": "Kate",
-        "level": 4}
-
-input_word = "philosophy"
-message = ""
-
-for sentence in english_sentences_damaged:
-    if type(sentence) == dict:
-        if user["level"] == int(sentence["level"]):
+def filter_out_sentences_by_input_word(input_word):
+    sentences_filtered_by_level = filter_out_sentences_by_level()
+    message = ""
+    if filter_out_sentences_by_level():
+        for sentence in sentences_filtered_by_level:
             if input_word.lower() in sentence.get("text").lower():
                 message = message + sentence.get("text") + "\n*****\n"
+        if message:
+            print(message)
+        else:
+            print(f"Sorry, there are no sentences with a word '{input_word}'")
     else:
-        continue
-if not message:
-    print(f"Sorry, there are no sentences with a word '{input_word}'")
-else:
-    print(message)
+        print(f"Sorry, there are no sentences for your level")
+    return message
+
+
+filter_out_sentences_by_input_word("the")
