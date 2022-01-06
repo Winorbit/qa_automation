01255eb8284a23421ddaab13b5fb54f85e2d416e
mandarynchik <95445536+mandarynchik@users.noreply.github.com>
2022-01-06 14:23:59 +0200

    Homework2 - functions
    #Домашка на функции
    #Вариант для level инвалид + опускать значение без ключа

--- python_base/english_bot.py
+++ python_base/english_bot.py
@@ -1,59 +1,47 @@
-english_sentences = [
-	{"text": "When my time comes \n Forget the wrong that I’ve done.", 
-	"level": 1},
-	{"text": "In a hole in the ground there lived a hobbit.", 
-	"level": 2},
-	{"text": "The sky the port was the color of television, tuned to a dead channel.", 
-	"level": 1},
-	{"text": "I love the smell of napalm in the morning.", 
-	"level": 0},
-	{"text": "The man in black fled across the desert, and the gunslinger followed.", 
-	"level": 0},
-	{"text": "The Consul watched as Kassad raised the death wand.", 
-	"level": 1},
-	{"text": "If you want to make enemies, try to change something.", 
-	"level": 2},
-	{"text": "We're not gonna take it. \n Oh no, we ain't gonna take it \nWe're not gonna take it anymore", 
-	"level": 1},
-	{"text":"I learned very early the difference between knowing the name of something and knowing something.", 
-	"level": 2}
-]
+#Домашка на функции
+#Вариант для level инвалид + опускать значение без ключа
 
-user = {"username": "Kate",
-		"level": 2}
+english_sentences_damaged = [{"text": "When my time comes \n Forget the wrong that I’ve done.", "level": 1},
+                             "In a hole in the ground there lived a hobbit.",
+                             {"text": "The sky the port was the color of television, tuned to a dead channel.",
+                              "level": "4"}, {"text": "I love the smell of napalm in the morning.", "level": 0},
+                             {"text": "The man in black fled across the desert, and the gunslinger followed.",
+                              "level": 0}, {"text": "The Consul watched as Kassad raised the death wand.", "level": 1},
+                             {"text": "If you want to make enemies, try to change something.", "level": 2}, {
+                                 "text": "We're not gonna take it. \n Oh no, we ain't gonna take it \nWe're not gonna take it anymore",
+                                 "level": 1}, {
+                                 "text": "I learned very early the difference between knowing the name of something and knowing something.",
+                                 "level": 2}]
+
+user = {"username": "Darya", "level": 1}
 
 input_word = "The"
-input_word=input_word.upper()
-message = ""
-for sentence in english_sentences:
-	if user["level"] == sentence["level"]:
-		if input_word in sentence.get("text").upper():
-			message = message+sentence.get("text")+"\n*****\n"
-if not message:
-	print("Sorry, go to hell.")
-else: 
-	print(message)
+input_word = input_word.upper()
+UserName = user.get("username")
+
+filtered = []
 
 
+def validate_input_data(raw_list):
+    for sentence in english_sentences_damaged:
+        if type(sentence) == str:
+            continue
+        if user.get("level") == int(sentence.get("level")):
+            if input_word in sentence.get("text").upper():
+                filtered.append(sentence.get("text"))
 
 
-###############################################
-english_sentences_damaged = [
-	{"text": "When my time comes \n Forget the wrong that I’ve done.", 
-	"level": 1},
-	"In a hole in the ground there lived a hobbit.", 
-	{"text": "The sky the port was the color of television, tuned to a dead channel.", 
-	"level": "4"},
-	{"text": "I love the smell of napalm in the morning.", 
-	"level": 0},
-	{"text": "The man in black fled across the desert, and the gunslinger followed.", 
-	"level": 0},
-	{"text": "The Consul watched as Kassad raised the death wand.", 
-	"level": 1},
-	{"text": "If you want to make enemies, try to change something.", 
-	"level": 2},
-	{"text": "We're not gonna take it. \n Oh no, we ain't gonna take it \nWe're not gonna take it anymore", 
-	"level": 1},
-	{"text":"I learned very early the difference between knowing the name of something and knowing something.", 
-	"level": 2}
-]
+validate_input_data(english_sentences_damaged)
+
+
+def create_and_send_message(filtered_list):
+    if len(filtered) == 0:
+        print(f"Sorry {UserName}, no sentences found")
+    if len(filtered) == 1:
+        print(f"Your sentence is: {filtered[0]}")
+    else:
+        message = ("\n*****\n".join(filtered))
+        print(message)
+
+
+create_and_send_message(filtered)
